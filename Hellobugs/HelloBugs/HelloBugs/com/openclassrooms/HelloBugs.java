0fabd81ae186a72816ff8c7a591b57bcdda713e6
typascal <typascal01@yahoo.ca>
2020-01-16 20:29:23 -0500

    Permier envoi du projet Hellobugs

---
+++ Hellobugs/HelloBugs/HelloBugs/com/openclassrooms/HelloBugs.java
@@ -0,0 +1,13 @@
+package com.openclassrooms;
+
+public class HelloBugs {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<5;i++) {
+		System.out.println("Hello buggy code!");
+		}
+				
+	}
+
+}
